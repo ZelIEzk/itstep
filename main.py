704556b52a24e90f2970eabd5524ce7edf354b98
Vladimir Zelik <kazantip81v@gmail.com>
2022-12-09 20:24:14 +0200

    123

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+from character import Character
+
+player1 = Character('Vasya', damage=10)
+player2 = Character('Petya', health=50)
+
+print(player1)
+print(player2)
+
+while player2.health > 0:
+    player1.attack(player2)
+
+print(player1)
+print(player2)
